86e6c012e1a39fe0a9c35284df9065cf59e3451d
상은 <1-blue98@naver.com>
2024-07-30 08:50:06 +0900

    Deploy: 마이크로소프트 클레어리티 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,14 @@ const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <html lang="ko" className="scroll-smooth">
       <head>
+        {/* 마이크로소프트 클레어리티 */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "nbx9pl1nx9");
+            })(window, document, "clarity", "script", "nf6gj21osg");
           `}
         </Script>
 
