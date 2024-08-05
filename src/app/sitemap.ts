0f0ae687abef506b0dce446d50e9a347e4e8921f
상은 <1-blue98@naver.com>
2024-08-05 20:13:42 +0900

    Deploy: 사이트맵 생성 로직 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,18 +6,26 @@ import type { IRoute } from "#/types";
 
 const allPosts = getAllPosts();
 
+/** 재귀적으로 돌아서 `sitemap` 생성 */
+const generateSitemap = (routes: IRoute[]): MetadataRoute.Sitemap => {
+  return routes.flatMap(({ path, sitemap, subRoutes }) => [
+    {
+      url: `${process.env.NEXT_PUBLIC_CLIENT_URL}${path}`,
+      priority: sitemap?.priority,
+      lastModified: sitemap?.lastmod,
+      changeFrequency: sitemap?.changefreq,
+    },
+    ...(subRoutes ? generateSitemap(subRoutes) : []), // 하위 경로에 대해 재귀 호출
+  ]);
+};
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    ...ROUTES.filter((route): route is Required<IRoute> => !!route.sitemap).map(
-      ({ path, sitemap }) => ({
-        url: `${process.env.NEXT_PUBLIC_CLIENT_URL}${path}`,
-        priority: sitemap.priority,
-        lastModified: sitemap.lastmod,
-        changeFrequency: sitemap.changefreq,
-      }),
+    ...generateSitemap(
+      ROUTES.filter((route): route is Required<IRoute> => !!route.sitemap),
     ),
     ...allPosts.map((postMetadata) => ({
-      url: `${process.env.NEXT_PUBLIC_CLIENT_URL}/${postMetadata.path}`,
+      url: `${process.env.NEXT_PUBLIC_CLIENT_URL}${postMetadata.path}`,
       priority: postMetadata.sitemap.priority,
       lastModified: postMetadata.sitemap.lastmod,
       changeFrequency: postMetadata.sitemap.changefreq,
